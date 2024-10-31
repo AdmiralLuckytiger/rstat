06b3874b64c8b6c305a2698105d7416eb615b13c
worker <Eduardo>
2024-10-31 09:46:29 +0100

    Add binary analyse functionality

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A basic shell command line called rstat, short for "Rust source statistics". It 
 It will generate a file count of Rust sources files, and source code metrics such as the number of blanks, comments, and actual lines of code within 
 the directory structure
 
-## Method of use: 
+## Method of use:
+
 For analise the current folder: `$> rstat -src .` 
 
 For print help: `$> rstat --help`
--- src/srcstats.rs
+++ src/srcstats.rs
@@ -2,9 +2,11 @@ pub mod errors;
 
 use std::ffi;
 use std::fs;
+use std::os::unix::fs::PermissionsExt;
 use std::path;
 use std::vec;
 use std::fmt;
+use binaryornot;
 
 use errors::StatsError;
 
@@ -16,6 +18,12 @@ pub struct SrcStats {
     blanks:             u32,
 }
 
+/// Binary medtrics definition
+pub struct BinStats {
+    number_of_files: u32,
+    weight:          u32,
+}
+
 impl fmt::Display for SrcStats {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, " >>> number_of_files: {},
@@ -25,7 +33,14 @@ impl fmt::Display for SrcStats {
          self.number_of_files, self.lines_of_code, self.comments, self.blanks)
     }
 }
- 
+
+impl fmt::Display for BinStats {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, " >>> number_of_files: {},
+                >>> weight: {} KB", self.number_of_files, self.weight)
+    }
+}
+
 /// Calculate source metrics for single file
 fn get_src_stats_for_file(file: &path::Path) -> Result<SrcStats, StatsError> {
     let file_contents = fs::read_to_string(file)?;
@@ -49,6 +64,17 @@ fn get_src_stats_for_file(file: &path::Path) -> Result<SrcStats, StatsError> {
         lines_of_code: loc,
         comments,
         blanks  
+    })
+}
+
+/// Calculate binary metrics for single file
+fn get_bin_stats_for_file(file: &path::Path) -> Result<BinStats, StatsError> {
+    let weight = file.metadata().unwrap().len()/1000;
+
+    Ok(BinStats {
+        number_of_files: 0,
+        weight: u32::try_from(weight)?,
+        
     })
 }
 
@@ -94,6 +120,55 @@ pub fn get_summary_src_stats(folder: &path::Path) -> Result<SrcStats, StatsError
         lines_of_code: total_loc,
         comments: total_comments,
         blanks: total_blanks,
+    })
+}
+
+/// Calculate binary metrics for all files in a directory root
+pub fn get_summary_bin_stats(folder: &path::Path) -> Result<BinStats, StatsError> {
+
+    let mut total_weight: u32 = 0;
+
+    let mut dir_entries: Vec<path::PathBuf> = vec![folder.to_path_buf()]; 
+    let mut bin_entries: Vec<fs::DirEntry> = vec![];
+
+    // Recursively iterate over directory entries to get flat
+    // list of binary file
+    while let Some(entry) = dir_entries.pop() {
+        for inner_entry in fs::read_dir(&entry)? {
+            if let Ok(entry) = inner_entry {
+                if entry.path().is_dir() {
+                    dir_entries.push(entry.path());
+                } else {
+                    if entry.path().extension() == None {
+                        match binaryornot::is_binary(entry.path()) {
+                            Ok(bool) => {
+                                if bool {
+                                    // Check if the file has the correct permissions
+                                    if entry.metadata()?.permissions().mode() & 0o111 != 0 {
+                                        bin_entries.push(entry);
+                                    }
+                                }
+                            },
+                            Err(_) => {},
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let bin_count = bin_entries.len();
+
+    // Compute stats
+    for entry in bin_entries {
+        let stat = get_bin_stats_for_file(&entry.path())?;
+
+        total_weight += stat.weight;
+    }
+
+    Ok(BinStats {
+        number_of_files: u32::try_from(bin_count)?,
+        weight: total_weight,
     })
 }
 
